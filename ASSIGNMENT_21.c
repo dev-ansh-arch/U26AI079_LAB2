78acdc7d81a3da81e8a9e170e3f5c5735d83a1ee
dev-ansh-arch <u26ai079@aid.svnit.ac.in>
2026-09-24 19:34:56 +0530

    Implement basic calculator functionality
    Added a simple calculator that performs addition, subtraction, multiplication, and division based on user input.

--- ASSIGNMENT_21.c
+++ ASSIGNMENT_21.c
@@ -1,4 +1,35 @@
 #include<stdio.h>
 int main(){
-    
+    int a,b;
+    float op;
+    printf("enter the 1st number : ");
+    scanf("%d",&a);
+    printf("enter the 2nd number : ");
+    scanf("%d",&b);
+
+    char operation;
+    printf("enter A for Addition, B for Substraction, C for Multipication, D for Division : ");
+    scanf(" %c",&operation);
+    switch(operation){
+        case 'A':
+        op=(a+b);
+        printf("SUM=%f",op);
+        break;
+        case 'B':
+        op=a-b;
+        printf("DIFFERENCE=%f",op);
+        break;
+        case 'C':
+        op=a*b;
+        printf("MULTIPLY=%f",op);
+        break;
+        case 'D':
+        op=a/(b+0.0);
+        printf("DIVIDE=%f",op);
+        break;
+        default:printf("invalid input");
+
+
+    }
+    return 0;
 }
